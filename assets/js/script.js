16799221d87d6533c4465ab05b8978116205e5f6
ByrdBassPC <byrdbass@gmail.com>
2022-01-09 21:50:18 -0600

    added style and got API to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,41 @@
 console.log("hello");
-var cityName = ""
-requestUrl = "api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=79ae4c44176953beec1155138bc60d35";
-redirectUrl = 
+apiKey = '79ae4c44176953beec1155138bc60d35'
+var cityName = "Austin"
+$(#autocomplete-input)
+//var requestUrl = "api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=79ae4c44176953beec1155138bc60d35";
+var url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey
+// redirectUrl = 
 
-fetch(requestUrl)
-    .then(function (response) {
-        if (response.status <= 400)
-        {
-            location.replace(redirectUrl)
-        } else {
-            console.log("hello");
-            return response.json();
+function getApi() {
+    fetch(url)
+        .then(function (response) {
+            console.log(response)
+
+            // if (response.status <= 400)
+            // {
+            //     location.replace(redirectUrl)
+            // } else {
+            //     console.log("hello");
+                return response.json();
+            })
+            .then(function (data) {
+                console.log("hello");
+                console.log(data)
+            //     //for loop??
+            // 
+            })
         }
-    });
+        getApi()
+
+$('#searchBtn').click(function() {
+    fetch(url)
+    
+})
+//insert city options here
+// var searchOptions =
+
+//SEARCH FEATURE TO PUT LIST OF CITIES INTO
+// document.addEventListener('DOMContentLoaded', function() {
+//     var searchInput = document.querySelectorAll('.autocomplete');
+//     var cityAutoComplete = M.Autocomplete.init(searchInput, searchOptions);
+// });
