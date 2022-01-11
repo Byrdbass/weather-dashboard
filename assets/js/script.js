b91fea2cb8305ed2aa43d584262470c5812f0001
ByrdBassPC <byrdbass@gmail.com>
2022-01-11 17:06:06 -0600

    got data to show up with lat and long

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,49 @@
-console.log("hello");
+console.log('hello');
 apiKey = '79ae4c44176953beec1155138bc60d35'
-var cityName = "Austin"
-$(#autocomplete-input)
+var cityName = 'austin';
+var latitude = '';
+var longitude = '';
 //var requestUrl = "api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=79ae4c44176953beec1155138bc60d35";
-var url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey
+
 // redirectUrl = 
 
-function getApi() {
-    fetch(url)
+//RUNS THE WHOLE SHABANG
+    function getCityWeather() {
+        var url = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + apiKey
+        
+        fetch(url)    
         .then(function (response) {
-            console.log(response)
+                console.log(response)
+                $('#searchBtn').click(function() {
+                    cityName = $('#autocomplete-input').val().trim();
+                    $('#autocomplete-input').html(cityName);
+                    console.log(cityName);
 
-            // if (response.status <= 400)
-            // {
-            //     location.replace(redirectUrl)
-            // } else {
-            //     console.log("hello");
-                return response.json();
-            })
-            .then(function (data) {
-                console.log("hello");
-                console.log(data)
-            //     //for loop??
-            // 
-            })
-        }
-        getApi()
+                })
+                // if (response.status <= 400)
+                // {
+                //     location.replace(redirectUrl)
+                // } else {
+                //     console.log("hello");
+                    return response.json();
+                })
+                .then(function (data) {
+                    console.log("hello");
+                    console.log(data)
+                    latitude = data.coord.lat;
+                    longitude = data.coord.lon;
+                    console.log(latitude + longitude)
+                //     //for loop??
+                // 
+                })
+            }
+    getCityWeather()
 
-$('#searchBtn').click(function() {
-    fetch(url)
+
+// $('#searchBtn').click(function() {
+//     fetch(url)
     
-})
+// })
 //insert city options here
 // var searchOptions =
 
